ea5ed342a6609a64552e8ada99c4a28f213d455c
discollizard <discollizard@github.com>
2022-09-26 08:08:30 -0300

    privacy page ready

--- api/resources/views/privacidade.blade.php
+++ api/resources/views/privacidade.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <section class="flex justify-center">
-    <div class="w-3/4">
-        <h1 class="text-3xl mb-2 text-center">Política de Privacidade</h1>
+    <div class="w-3/4 leading-6 mt-5">
+        <h1 class="text-xl font-bold md:text-3xl mb-2 text-center">Política de Privacidade</h1>
         <p class="mb-2">
             A sua privacidade é importante para nós.
             É política do GiriAPI respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href="/" class="text-blue-500">GiriAPI</a>,
@@ -17,7 +17,7 @@
             Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.
         </p>
         
-        <p class="mb-2">
+        <p class="mb-5">
             O nosso site pode ter links para sites externos que não são operados por nós.
             Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.
             Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.
@@ -25,19 +25,20 @@
              Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contato conosco.
         </p>
 
-        <h1 class="text-3xl mb-2 text-center">Política de Privacidade</h1>
-        
+        <h1 class="text-xl font-bold md:text-3xl mb-2 text-center">Compromisso do Usuário</h1>
 
-        Compromisso do Usuário
-        O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o GiriAPI oferece no site e com caráter enunciativo, mas não limitativo:
+        O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o GiriAPI oferece no site e com caráter enunciativo, mas não limitativo:<br>
 
-        A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;
-        B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, casas de apostas ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;
-        C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do GiriAPI, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.
-        Mais informações
-        Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.
+        <p class="mb-5">
+            <b>A)</b> Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;<br>
+            <b>B)</b> Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, casas de apostas ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;<br>
+            <b>C)</b> Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do GiriAPI, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.<br>
+        </p>
+        <hr class='mb-5'>
+        <h2 class="text-xl font-bold md:text-3xl mb-2 text-center">Mais informações</h2>
+        Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.<br>
 
-        Esta política é efetiva a partir de 24 setembro 2022 11:47
+       <h3 class="text-center text-2xl my-5">Esta política é efetiva a partir de 24 setembro 2022 11:47</h3>
     </div>
 </section>
 @endsection
