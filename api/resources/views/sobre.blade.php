b9c538956bcee78de9c6d5148101622fb8c146b4
alvaroascenco <alvaroascenco@github.com>
2022-10-06 08:40:25 -0300

    github link added

--- api/resources/views/sobre.blade.php
+++ api/resources/views/sobre.blade.php
@@ -11,11 +11,12 @@
                   <h3 class='text-xl mb-5'>
                      Contribuidores
                   </h3>
-                  <ul class='leading-6 list-disc'>
+                  <ul class='leading-6 list-disc mb-5'>
                     <li> Alvaro Coslovsky (<a href='https://github.com/discollizard' target='_blank' rel='noopener norefferer' class='text-blue-500 hover:underline'>discollizard</a>) - Idealização, desenvolvimento fullstack e gerência do projeto</li>
                     <li> <a href='https://github.com/gabriel-baltazar' rel='noopener norefferer' target='_blank' class='text-blue-500 hover:underline'>Gabriel Baltazar</a> - Desenvovimento back-end </li>
                     <li> Mickelly Coslovsky - Design de elementos visuais</li>
                   </ul>
+                  <span class="text-blue-500 hover:underline"><a target='_blank' rel='noopener norefferer' href="https://github.com/discollizard/giriapp">Gostaria de contribuir?</a></span>
                </div>
                <div class="border-t-2 md:border-l-2 md:h-56 h-0 w-56 md:w-0 border-slate-500"></div>
                <div class='mr-5 w-full md:w-1/2 mt-3 md:mt-0 md:pl-5'>
