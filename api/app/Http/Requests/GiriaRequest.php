8863483f17f3e25278eb9a573f40bea77e87c03b
discollizard <discollizard@github.com>
2023-02-03 06:21:57 -0300

    telescope + validation progress

--- api/app/Http/Requests/GiriaRequest.php
+++ api/app/Http/Requests/GiriaRequest.php
@@ -25,13 +25,13 @@ class GiriaRequest extends FormRequest
     public function rules()
     {
         return [
-            'nome' => 'required|max:255|regex:/^[A-Za-zÀ-ÖØ-öø-ÿçÇ.,\-\s]{2,}$/',
+            'nome' => 'required|max:255|regex:/^[A-Za-zÀ-ÖØ-öø-ÿçÇ.\(\),\-\s]{2,}$/',
             'local' => 'required|array|min:1',
             'local.*' => 'required|max:255|regex:/^[A-Za-zÀ-ÖØ-öø-ÿçÇ.,\-\s]{2,}$/',
             'significado' => 'required|array|min:1',
-            'significado.*' => 'required|max:1024|regex:/^[A-Za-zÀ-ÖØ-öø-ÿçÇ.,\s\-]{2,}$/',
-            'etimologia' => 'nullable|max:1024|regex:/^[A-Za-zÀ-ÖØ-öø-ÿçÇ.,\-\s]{2,}$/',
-            'imagem' => 'nullable|max:255|regex:/^https?\:\/\/[A-Za-z0-9.\-\/_\?\=]{2,}$/',
+            'significado.*' => 'required|max:1024|regex:/^[A-Za-zÀ-ÖØ-öø-ÿçÇ\.\,\(\)\s\-]{2,}$/',
+            'etimologia' => 'nullable|max:1024|regex:/^[A-Za-zÀ-ÖØ-öø-ÿçÇ.,\(\)\-\s]{2,}$/',
+            // 'imagem' => 'nullable|max:255|regex:/^https?\:\/\/[A-Za-z0-9.\-\/_\?\=]{2,}$/',
             'url' => 'nullable|max:255|regex:/^https\:\/\/(www\.)?youtube\.com\/watch\?v\=[a-zA-Z0-9_-]{11}$/'
         ];
     }
